4e5afb5c0350550667f83731816c785ce91e2c57
EshanRS <eshanratnesh@gmail.com>
2025-04-05 01:09:47 +0530

    Sign-in Page finalized

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,17 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        background: 'linear-gradient(to right,rgb(0, 43, 80),rgb(150, 242, 247))',
+      }}
+    >
+      <SignIn />
+      </div>
+  )
 }
