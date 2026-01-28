a15c6b0fe5fc1c79b9743030013d34c49bb97f31
shiva <114084628+shivaKhanduri@users.noreply.github.com>
2026-01-28 18:23:27 +0530

    trial 4

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,39 +1,14 @@
 "use client"
 
-import * as React from "react"
-import dynamic from 'next/dynamic'
-
-// Dynamically import providers with no SSR to completely avoid prerendering issues
-const ClerkProviderWrapper = dynamic(
-  () => import('@clerk/nextjs').then(mod => {
-    const { ClerkProvider } = mod
-    return function ClerkWrapper({ children }: { children: React.ReactNode }) {
-      return <ClerkProvider>{children}</ClerkProvider>
-    }
-  }),
-  { ssr: false }
-)
-
-const ThemeProviderWrapper = dynamic(
-  () => import('next-themes').then(mod => {
-    const { ThemeProvider } = mod
-    return function ThemeWrapper({ children }: { children: React.ReactNode }) {
-      return (
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-          {children}
-        </ThemeProvider>
-      )
-    }
-  }),
-  { ssr: false }
-)
+import { ClerkProvider } from "@clerk/nextjs"
+import { ThemeProvider } from "next-themes"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProviderWrapper>
-      <ThemeProviderWrapper>
+    <ClerkProvider>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
         {children}
-      </ThemeProviderWrapper>
-    </ClerkProviderWrapper>
+      </ThemeProvider>
+    </ClerkProvider>
   )
 }
